203b702eed7121d21efd1f298b460a62165d78d7
Ramiro Trejo <trejoramiro1@gmail.com>
2016-11-23 19:46:23 -0600

    Delete comment

--- app/main.jsx
+++ app/main.jsx
@@ -4,5 +4,4 @@ import App from './app.jsx';
 import Backend from './backend';
 
 var backend = new Backend();
-// console.log(backend);
 ReactDOM.render(<App backend={backend} />, document.querySelector('.react'));

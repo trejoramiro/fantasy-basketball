890793cf519e7290a639a98177b15c6b4601e91a
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-21 11:05:56 -0600

    Edit file

--- app/backend.js
+++ app/backend.js
@@ -1,16 +1,17 @@
 export default class Backend {
   constructor() {
-    //make ajax call to go server url: 'data'
-    this.team = [];
+    this.team = [{id: 1, firstname: "Lebron", lastname: "James", pts: 30, ast: 15, reb: 6, position: "Power Forward" },{id: 2, firstname: "Russel", lastname: "Westbrook", pts: 31, ast: 14, reb: 10, position: "Point Guard"},{id: 3, firstname: "Kevin", lastname: "Durant", pts: 27, ast: 12, reb: 8, position: "Gaurd" }];
+    this.players;
+
   };
+
 
    loadDataFromServer() {
      var _this = this
-     fetch('data').then(function(response){
-      //  console.log(response.json());
+     fetch('search').then(function(response){
        return response.json();
      }).then(function(data){
-       _this.team = data;
+       _this.players = data;
      });
    }
 

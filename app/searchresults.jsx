25a21b203595fd824d3ae3e13c4b35b92ee198e5
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-21 11:08:29 -0600

    Bind event handlers

--- app/searchresults.jsx
+++ app/searchresults.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
 class SearchResults extends React.Component {
+
+  addTeamMember(item) {
+    this.props.addTeamMember(item);
+  }
+
+  performAnotherSearch(url) {
+    this.props.performOtherSearch(url);
+  }
+
   render() {
     return (
         <div>
           <div className="row">
             <div className="col-md-12">
-          <table className="table table-bordered">
+          <table className="table table-bordered table-striped">
             <tbody>
             <tr>
-              <th>Id</th>
               <th>First Name</th>
               <th>Last Name</th>
               <th>Position</th>
@@ -19,17 +27,14 @@ class SearchResults extends React.Component {
               <th>Action</th>
             </tr>
           </tbody>
-            { this.props.team.map((x) => { return (
+            { this.props.players.map((x) => { return (
               <tbody key={x.id}>
               <tr>
                 <td>
-                  { x.id }
+                  { x.firstname }
                 </td>
                 <td>
-                  { x.firstName }
-                </td>
-                <td>
-                  { x.lastName }
+                  { x.lastname }
                 </td>
                 <td>
                   { x.position }
@@ -44,7 +49,7 @@ class SearchResults extends React.Component {
                   { x.reb }
                 </td>
                 <td>
-                  <button className="btn btn-success" href="#" type="submit">Add</button>
+                  <button className="btn btn-success" onClick={this.addTeamMember.bind(this, x)} type="button">Add</button>
                 </td>
               </tr>
             </tbody>
@@ -53,17 +58,12 @@ class SearchResults extends React.Component {
         <nav aria-label="Page navigation">
           <ul className="pagination">
             <li>
-              <a href="#" aria-label="Previous">
+              <a role="button" onClick={this.performAnotherSearch.bind(this, this.props.links.prev)} aria-label="Previous">
                 <span aria-hidden="true">&laquo;</span>
               </a>
             </li>
-            <li><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
             <li>
-              <a href="#" aria-label="Next">
+              <a role="button" onClick={this.performAnotherSearch.bind(this, this.props.links.next)} aria-label="Next">
                 <span aria-hidden="true">&raquo;</span>
               </a>
             </li>

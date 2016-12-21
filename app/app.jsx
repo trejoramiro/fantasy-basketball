5c2ea13e782704236026b664ec12cd0cbfbc8472
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-21 11:04:24 -0600

    Add functions and props

--- app/app.jsx
+++ app/app.jsx
@@ -7,9 +7,68 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      "teams": []
+      "total": 0,
+      "players": [],
+      "team": [ { id: 1, firstname: "Lebron", lastname: "James", pts: 30, ast: 15, reb: 6, position: "Power Forward" },{id: 2, firstname: "Russell", lastname: "Westbrook", pts: 31, ast: 14, reb: 10, position: "Point Guard"},{id: 3, firstname: "Kevin", lastname: "Durant", pts: 27, ast: 12, reb: 8, position: "Gaurd" } ],
+      "links": [ { prev: '#', next: '#' } ],
     };
+
+    this.removeTeamMember = this.removeTeamMember.bind(this);
+    this.addTeamMember = this.addTeamMember.bind(this);
+    this.performSearch = this.performSearch.bind(this);
+    this.performOtherSearch = this.performOtherSearch.bind(this);
   }
+
+
+  componentDidMount() {
+
+  }
+
+
+  performSearch(item) {
+    var url = 'search?type=' + item.type + '&text=' + item.text + '&offset=0'
+    var _this = this;
+    fetch(url).then(function(response){
+      return response.json();
+    }).then(function(d){
+      console.log(d);
+      _this.setState({ players: d[0].data, links: { prev: d[0].link.prev , next: d[0].link.next } });
+    });
+  }
+
+
+  performOtherSearch(url) {
+    var _this = this;
+    fetch(url).then(function(response){
+      return response.json();
+    }).then(function(d){
+      console.log(d);
+      _this.setState({ players: d[0].data, links: { prev: d[0].link.prev , next: d[0].link.next } });
+    });
+  }
+
+
+  updateTotal() {
+    var newTotal = this.state.team.reduce(function(total, item){
+      return total + item.pts;
+    }, 0)
+    this.setState({ total: newTotal });
+  }
+
+
+  removeTeamMember(index) {
+    var teamMembers = this.state.team.filter(function(item) {
+      return item.id !== index;
+    });
+    this.setState({ team: teamMembers });
+  }
+
+
+  addTeamMember(newTeamMember) {
+    this.setState({ team: this.state.team.concat([newTeamMember])});
+    this.updateTotal();
+  }
+
 
   render() {
     return (
@@ -21,9 +80,10 @@ class App extends React.Component {
                 </div>
               </div>
             </nav>
-            <SearchBar />
-            <SearchResults team={this.props.backend.team} />
-            <Team team={this.props.backend.team}/>
+            <SearchBar performSearch={this.performSearch.bind(this)} />
+            <SearchResults links={this.state.links} players={this.state.players} addTeamMember={this.addTeamMember.bind(this)} performOtherSearch={this.performOtherSearch.bind(this)}/>
+            <h1>{this.state.total}</h1>
+            <Team team={this.state.team} removeTeamMember={this.removeTeamMember.bind(this)}/>
       </div> )
     }
 }

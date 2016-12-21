3cdba5effbf2902e496b0c598784f17850765f26
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-21 11:06:29 -0600

    Remove backend props

--- app/main.jsx
+++ app/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
-import Backend from './backend';
 
-var backend = new Backend();
-console.log("hello ramiro")
-backend.loadDataFromServer();
-ReactDOM.render(<App backend={backend} />, document.querySelector('.react'));
+
+
+ReactDOM.render(<App />, document.querySelector('.react'));

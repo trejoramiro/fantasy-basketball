d8eed0da384f6bd098cba93642c5ec5c8117b065
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-27 20:25:54 -0600

    Add dropdown select options

--- app/searchbar.jsx
+++ app/searchbar.jsx
@@ -27,7 +27,14 @@ class SearchBar extends React.Component {
   }
 
   handleTypeChange(e) {
-    this.setState({ type: e.target.value });
+    let newType = e.target.value;
+    if(newType == 'position') {
+      this.setState({ type: newType, text: 'Center' });
+    } else if(newType == 'team') {
+      this.setState({ type: newType, text: 'ATL' });
+    } else {
+      this.setState({ type: newType, text: '' });
+    }
   }
 
   render() {
@@ -35,18 +42,47 @@ class SearchBar extends React.Component {
     let input = null;
     if (this.state.type == 'position') {
       input = <select className="form-control" value={this.state.text} onChange={this.handleTextChange}>
-        <option value="Center">Center</option>
+        <option selected value="Center">Center</option>
+        <option value="Center-Forward">Center-Forward</option>
+        <option value="Forward">Forward</option>
+        <option value="Forward-Center">Forward-Center</option>
+        <option value="Forward-Guard">Forward-Guard</option>
         <option value="Guard">Guard</option>
-        <option value="power_forward">Power Forward</option>
-        <option value="point_guard">Point Guard</option>
+        <option value="Guard-Forward">Guard-Forward</option>
       </select>;
 
     } else if (this.state.type == 'team') {
       input = <select className="form-control" value={this.state.text} onChange={this.handleTextChange}>
-        <option value="CHI">CHI</option>
+        <option selected value="ATL">ATL</option>
+        <option value="BKN">LAL</option>
+        <option value="BOS">MIA</option>
+        <option value="CHA">NYC</option>
+        <option value="CHI">LAL</option>
+        <option value="CLE">MIA</option>
+        <option value="DAL">NYC</option>
+        <option value="DEN">LAL</option>
+        <option value="DET">MIA</option>
+        <option value="GSW">NYC</option>
+        <option value="HOU">LAL</option>
+        <option value="IND">MIA</option>
+        <option value="LAC">NYC</option>
         <option value="LAL">LAL</option>
-        <option value="MIA">MIA</option>
-        <option value="NYC">NYC</option>
+        <option value="MEM">MIA</option>
+        <option value="MIA">NYC</option>
+        <option value="MIL">LAL</option>
+        <option value="MIN">MIA</option>
+        <option value="NOP">NYC</option>
+        <option value="NYK">LAL</option>
+        <option value="OKC">MIA</option>
+        <option value="ORL">NYC</option>
+        <option value="PHI">LAL</option>
+        <option value="PHX">MIA</option>
+        <option value="POR">NYC</option>
+        <option value="SAC">MIA</option>
+        <option value="SAS">NYC</option>
+        <option value="TOR">LAL</option>
+        <option value="UTA">MIA</option>
+        <option value="WAS">NYC</option>
       </select>;
 
     } else {
